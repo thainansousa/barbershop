b14404b30d319286808bdb7607729f1b14c483e3
thainansousa <thainansousa7@gmail.com>
2020-06-28 15:43:00 -0300

    página de login com react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '../public/img/barbershop.svg';
+import '../public/img/user.svg.svg';
+import '../public/img/password.svg';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('body')
+);
